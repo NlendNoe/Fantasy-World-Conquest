7714da0c63cf84100e0ab1b3df14e1c29588672e
unknown <noepouth02@mail.com.com>
2026-09-09 12:30:30 +0100

    Amelioration de l'interface graphique et ajout de nouvelles fonctionnalit├®s

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main()
     }
 
     sf::Texture textureFond;
-    bool fondCharge = textureFond.loadFromFile("assets/fonts/PixelGamer-Regular.otf");
+    bool fondCharge = textureFond.loadFromFile("assets/backgrounds/fondPince-2.jpg");
     sf::Sprite spriteFond;
     if (fondCharge)
     {
